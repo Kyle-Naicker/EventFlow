79646f96617005d31f2e5b43be44d4613fb3e7c4
Kyle-Naicker <kylenaicker05@gmail.com>
2026-08-30 00:18:53 +0200

    Created Notice Class

---
+++ Notice.h
@@ -0,0 +1,27 @@
+#ifndef NOTICE_H
+#define NOTICE_H
+
+#include <string>
+
+enum class NoticeType {
+    OPEN,
+    CLOSE,
+    SCHEDULE_CHANGE,
+    CAPACITY_ALERT,
+    PAUSE,
+    RESUME,
+    EVACUATE,
+    VENUE_CHANGE,
+    POWER_OUTAGE,
+    CANCEL
+};
+
+struct Notice {
+    NoticeType type;
+    std::string message;
+    int capacityThreshold = -1;
+    Notice() : type(NoticeType::SCHEDULE_CHANGE), message(""), capacityThreshold(-1){}
+    Notice(NoticeType t, const std::string& msg, int threshold = -1) : type(t), message(msg), capacityThreshold(threshold){}
+};
+
+#endif
