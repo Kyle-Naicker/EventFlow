2e903c6d7eaeab163b938caf6fcb7412cb98bb18
HP <haythamphillips@gmail.com>
2026-08-30 00:50:47 +0200

    Implemented more concrete leaves

--- ConcreteLeaves.cpp
+++ ConcreteLeaves.cpp
@@ -26,3 +26,64 @@ void LectureHall::reactToNotice(const Notice& notice) {
             break;
     }
 }
+
+
+
+WorkshopRoom::WorkshopRoom(const std::string& name,int capacity,bool indoors):EventUnit(name, capacity, indoors) {}
+
+
+void WorkshopRoom::reactToNotice(const Notice& notice) {
+    switch(notice.type) {
+        case NoticeType::EVACUATE:
+            isPaused_=true;
+            std::cout<<"  [WorkshopRoom:"<<name_<<"] hands-on activity stopped immediately (evacuation)\n";
+            break;
+        case NoticeType::PAUSE:
+            if(indoors_){
+                std::cout<<"  [WorkshopRoom:"<< name_<< "] indoors, continuing session through pause\n";
+            }else{
+                isPaused_=true;
+                std::cout << "  [WorkshopRoom:" << name_ << "] outdoor session paused\n";
+            }
+            break;
+        case NoticeType::RESUME:
+            isPaused_=false;
+            std::cout<<"  [WorkshopRoom:"<<name_<< "] session resumed\n";
+            break;
+        case NoticeType::SCHEDULE_CHANGE:
+            std::cout<<"  [WorkshopRoom:"<<name_<< "] re-planning exercises for new slot\n";
+            break;
+        case NoticeType::CLOSE:
+        case NoticeType::CANCEL:
+            isOpen_=false;
+            std::cout<<"  [WorkshopRoom:"<<name_<<"] room closed\n";
+            break;
+        default:
+            break;
+    }
+}
+
+
+RegistrationDesk::RegistrationDesk(const std::string& name, int capacity, bool indoors):EventUnit(name, capacity, indoors) {}
+
+void RegistrationDesk::reactToNotice(const Notice& notice) {
+    switch (notice.type) {
+        case NoticeType::CLOSE:
+        case NoticeType::EVACUATE:
+        case NoticeType::CANCEL:
+            isOpen_=false;
+            std::cout<<"  [RegistrationDesk:"<<name_<<"] badge issuing stopped\n";
+            break;
+        case NoticeType::RESUME:
+        case NoticeType::OPEN:
+            isOpen_=true;
+            std::cout<<"  [RegistrationDesk:"<<name_<<"] badge issuing resumed\n";
+            break;
+        case NoticeType::CAPACITY_ALERT:
+            
+            std::cout<<"  [RegistrationDesk:"<<name_<< "] capacity alert noted, no action needed here\n";
+            break;
+        default:
+            break;
+    }
+}
