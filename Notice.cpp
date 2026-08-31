2b365cd7f35f60978fc66cd89379093be1a92352
Kyle-Naicker <kylenaicker05@gmail.com>
2026-08-31 17:14:33 +0200

    Added SCHEDULE_CHANGE to the switch

--- Notice.cpp
+++ Notice.cpp
@@ -6,16 +6,18 @@ const char* noticeTypeName(NoticeType type){
             return "OPEN";
         case NoticeType::CLOSE:
             return "CLOSE";
+        case NoticeType::SCHEDULE_CHANGE:
+            return "SCHEDULE_CHANGE";
         case NoticeType::CAPACITY_ALERT:
             return "CAPACITY_ALERT";
-        case NoticeType::WEATHER_ALERT:
-            return "WEATHER_ALERT";
         case NoticeType::PAUSE:
             return "PAUSE";
         case NoticeType::RESUME:
             return "RESUME";
         case NoticeType::EVACUATE:
             return "EVACUATE";
+        case NoticeType::WEATHER_ALERT:
+            return "WEATHER_ALERT";
         case NoticeType::POWER_OUTAGE:
             return "POWER_OUTAGE";
         case NoticeType::CANCEL:
